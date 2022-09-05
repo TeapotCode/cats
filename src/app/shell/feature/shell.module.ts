9a4f374b6ced4205dc7c4922dbf842ceb0defeb4
FilipCichowski <58987436+FilipCichowski@users.noreply.github.com>
2022-09-05 11:28:31 +0200

    Added navbar

--- src/app/shell/feature/shell.module.ts
+++ src/app/shell/feature/shell.module.ts
@@ -1,5 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import {MatBadgeModule} from "@angular/material/badge";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatToolbarModule} from "@angular/material/toolbar";
 
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
@@ -18,6 +22,10 @@ import { NavbarComponent } from '../ui/navbar/navbar.component';
       maxAge: 25,
       logOnly: environment.production,
     }),
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatBadgeModule,
   ],
 })
 export class ShellModule {}
