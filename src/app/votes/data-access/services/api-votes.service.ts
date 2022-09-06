34d9cfc007f8cba7eb5dcaca5284ffabd2f00b2c
ProgramistaJarek <dojarka0@gmail.com>
2022-09-06 19:59:43 +0200

    refactoring

--- src/app/votes/data-access/services/api-votes.service.ts
+++ src/app/votes/data-access/services/api-votes.service.ts
@@ -1,41 +1,46 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 import { Store } from '@ngrx/store';
 
+import * as catsSelectors from '../../../shell/data-access/cats.selector';
 import { voteImages } from '../../utilities/votesImages.model';
+import {
+  loadFromApi,
+  setImagesWithVote,
+} from '../../../shell/data-access/cats.action';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiVotesService {
-  constructor(private store: Store) {}
+  constructor(private store: Store, private http: HttpClient) {}
 
-  // getVotedImagesWithFavorites() {
-  //   // return this.store.dispatch(setImagesWithVoteAndFavourites());
-  // }
+  getVotedImages(): Observable<voteImages[]> {
+    this.store.dispatch(loadFromApi());
+    return this.store.select(catsSelectors.selectVotedImages);
+  }
 
-  // getVotedImages(): Observable<voteImages[]> {
-  //   // return this.store.select(catsSelectors.selectVotedImages);
-  // }
+  selectOnlyPositivieImage(): Observable<voteImages[]> {
+    return this.store.select(catsSelectors.selectOnlyPositivieImage);
+  }
 
-  // selectOnlyPositivieImage(): Observable<voteImages[]> {
-  //   // return this.store.select(catsSelectors.selectOnlyPositivieImage);
-  // }
+  selectOnlyNegativeImage(): Observable<voteImages[]> {
+    return this.store.select(catsSelectors.selectOnlyNegativeImage);
+  }
 
-  // selectOnlyNegativeImage(): Observable<voteImages[]> {
-  //   // return this.store.select(catsSelectors.selectOnlyNegativeImage);
-  // }
+  dislikeImage(fav_id: number) {
+    return this.http.delete(
+      `https://api.thecatapi.com/v1/favourites/${fav_id}`
+    );
+  }
 
-  // dislikeImage(fav_id: number) {
-  //   // this.store.dispatch(deleteImageFromFavourits({ fav_id }));
-  // }
+  likeImage(image_id: string) {
+    console.log(image_id);
+  }
 
-  // likeImage(image_id: string) {
-  //   this.store.dispatch(likeImage({ imageId: image_id }));
-  // }
-
-  // deleteVote(vote_id: number) {
-  //   this.store.dispatch(removeVote({ voteId: vote_id }));
-  // }
+  deleteImageFromVote(vote_id: number) {
+    console.log(vote_id);
+  }
 }
---
+++ src/app/votes/data-access/votes.store.ts
@@ -0,0 +1,115 @@
+import { Injectable } from '@angular/core';
+import { ComponentStore } from '@ngrx/component-store';
+import { Observable, switchMap, tap } from 'rxjs';
+
+import { ApiVotesService } from './services/api-votes.service';
+import { ApiHomeService } from '../../shell/data-access/api-cats.service';
+import { voteImages } from '../utilities/votesImages.model';
+
+export interface VotesState {
+  image: voteImages[];
+}
+
+@Injectable()
+export class VotesStore extends ComponentStore<VotesState> {
+  constructor(private api: ApiVotesService, private apiCats: ApiHomeService) {
+    super({ image: [] });
+  }
+
+  //reducers
+  protected updateImages = this.updater((state, image: voteImages[]) => ({
+    image,
+  }));
+
+  protected updateImagesIfDislike = this.updater((state, id: number) => {
+    const newImage = state.image.map((img) =>
+      img.favoriteId === id ? { ...img, isFavorite: false } : img
+    );
+    return {
+      ...state,
+      image: newImage,
+    };
+  });
+
+  protected updateImagesIfLike = this.updater((state, id: string) => {
+    const newImage = state.image.map((img) =>
+      img.image_id === id ? { ...img, isFavorite: true } : img
+    );
+    return {
+      ...state,
+      image: newImage,
+    };
+  });
+
+  protected updateImagesIfDelete = this.updater((state, id: number) => {
+    const newImage = state.image.filter((img) => img.id !== id);
+    return {
+      ...state,
+      image: newImage,
+    };
+  });
+
+  //selectors
+  selectImagesPositive(): Observable<voteImages[]> {
+    return this.select((state) => state.image.filter((img) => img.value === 1));
+  }
+
+  selectImagesNegative(): Observable<voteImages[]> {
+    return this.select((state) =>
+      state.image.filter((img) => img.value === -1)
+    );
+  }
+
+  //effects
+  readonly getImages = this.effect((params$: Observable<unknown>) => {
+    return params$.pipe(
+      switchMap((_) =>
+        this.api.getVotedImages().pipe(
+          tap({
+            next: (result) => this.updateImages(result),
+            error: (e) => console.log(e),
+          })
+        )
+      )
+    );
+  });
+
+  readonly dislikeImage = this.effect((id$: Observable<number>) => {
+    return id$.pipe(
+      switchMap((id) =>
+        this.apiCats.removeFavorite(id).pipe(
+          tap({
+            next: (result) => this.updateImagesIfDislike(id),
+            error: (e) => console.log(e),
+          })
+        )
+      )
+    );
+  });
+
+  readonly likeImage = this.effect((id$: Observable<string>) => {
+    return id$.pipe(
+      switchMap((id) =>
+        this.apiCats.setFavorite(id).pipe(
+          tap({
+            next: (result) => this.updateImagesIfLike(id),
+            error: (e) => console.log(e),
+          })
+        )
+      )
+    );
+  });
+
+  readonly deleteImageVote = this.effect((id$: Observable<number>) => {
+    return id$.pipe(
+      switchMap((id) =>
+        this.apiCats.removeVote(id).pipe(
+          tap({
+            next: (result) => this.updateImagesIfDelete(id),
+            error: (e) => console.log(e),
+          })
+        )
+      )
+    );
+  });
+}
