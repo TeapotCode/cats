221cca8cb21941f8098f6f2abe9931147e38f750
Adrian Czaicki <adrianrobimuzyke@gmail.com>
2022-09-07 16:26:34 +0200

    add breeds and category filter

--- src/app/home/data-access/api-home.service.ts
+++ src/app/home/data-access/api-home.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Image } from '../utils/image.interface';
+import { Params } from '@angular/router';
+import { Category } from '../utils/category.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -8,9 +10,19 @@ import { Image } from '../utils/image.interface';
 export class ApiHomeService {
   constructor(private httpClient: HttpClient) {}
 
-  getImages(number: number) {
+  getImages(
+    number: number,
+    categoryId?: number | null,
+    breedId?: number | null
+  ) {
+    let params = new HttpParams();
+    if (categoryId) params = params.append('category_ids', categoryId);
+
+    if (breedId) params = params.append('breed_ids', breedId);
+
     return this.httpClient.get<Image[]>(
-      `https://api.thecatapi.com/v1/images/search?limit=${number}`
+      `https://api.thecatapi.com/v1/images/search?limit=${number}`,
+      { params }
     );
   }
 
@@ -39,6 +51,18 @@ export class ApiHomeService {
   removeFavorite(favoriteId: number) {
     return this.httpClient.delete(
       `https://api.thecatapi.com/v1/favourites/${favoriteId}`
+    );
+  }
+
+  getCategories() {
+    return this.httpClient.get<Category[]>(
+      'https://api.thecatapi.com/v1/categories'
+    );
+  }
+
+  getBreeds() {
+    return this.httpClient.get<Category[]>(
+      'https://api.thecatapi.com/v1/breeds'
     );
   }
 }
