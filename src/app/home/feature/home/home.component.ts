2dd5037d6902fd359cfc7c97c082a80afcb8ab26
Adrian Czaicki <adrianrobimuzyke@gmail.com>
2022-09-07 11:02:49 +0200

    add infinite scrolling

--- src/app/home/feature/home/home.component.ts
+++ src/app/home/feature/home/home.component.ts
@@ -1,17 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  NgZone,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { Store } from '@ngrx/store';
 import { loadPhotos } from '../../data-access/home.action';
 import { selectImages } from '../../data-access/home.selector';
 import { RandomImage } from '../../utils/randomImage.interface';
+import { filter, map, pairwise, throttleTime } from 'rxjs';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent implements OnInit {
-  constructor(private store: Store) {}
+export class HomeComponent implements OnInit, AfterViewInit {
+  constructor(private store: Store, private ngZone: NgZone) {}
+  ngAfterViewInit(): void {
+    this.scroller
+      .elementScrolled()
+      .pipe(
+        map(() => this.scroller.measureScrollOffset('bottom')),
+        pairwise(),
+        filter(([y1, y2]) => y2 < y1 && y2 < 140),
+        throttleTime(200)
+      )
+      .subscribe(() => {
+        this.ngZone.run(() => {
+          this.store.dispatch(loadPhotos());
+        });
+      });
+  }
 
   images$ = this.store.select(selectImages);
+
+  @ViewChild('scroller') scroller!: CdkVirtualScrollViewport;
 
   ngOnInit(): void {
     this.store.dispatch(loadPhotos());
