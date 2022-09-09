45e857aa050fb03a1e2f4369a20fc73b5db3c2b5
FilipCichowski <58987436+FilipCichowski@users.noreply.github.com>
2022-09-09 14:01:07 +0200

    Added test for side effect

--- src/app/upload/data/access/state/upload.effects.spec.ts
+++ src/app/upload/data/access/state/upload.effects.spec.ts
@@ -1,16 +1,19 @@
-import {TestBed} from "@angular/core/testing";
-import {MatSnackBar, MatSnackBarModule} from "@angular/material/snack-bar";
-import {provideMockActions} from "@ngrx/effects/testing";
-import {MockStore, provideMockStore} from "@ngrx/store/testing";
-import {Observable, of} from "rxjs";
-import {displayServerError} from "./upload.actions";
-import {UploadEffects} from "./upload.effects";
-import {provideMock} from "@testing-library/angular/jest-utils";
+import { UploadService } from './../upload.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { provideMockActions } from '@ngrx/effects/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { Observable, of } from 'rxjs';
+import { displayServerError } from './upload.actions';
+import { UploadEffects } from './upload.effects';
+import { provideMock } from '@testing-library/angular/jest-utils';
 
 let actions$: Observable<any>;
 let store: MockStore;
 let effects: any;
 let snackBar: any;
+let uploadService: any;
 
 beforeEach(() => {
   TestBed.configureTestingModule({
@@ -18,23 +21,30 @@ beforeEach(() => {
       UploadEffects,
       provideMockStore({}),
       provideMockActions(() => actions$),
-      provideMock()
+      provideMock(MatSnackBar),
+      provideMock(UploadService),
     ],
-    imports: [
-      MatSnackBarModule
-    ]
-  })
+    imports: [MatSnackBarModule, HttpClientTestingModule],
+  });
 
   store = TestBed.inject(MockStore);
-  effects = TestBed.inject(UploadEffects)
-  snackBar = TestBed.inject(SnackBar)
-})
+  effects = TestBed.inject(UploadEffects);
+  snackBar = TestBed.inject(MatSnackBar);
+  uploadService = TestBed.inject(UploadService);
+});
 
 describe('UploadEffects', () => {
   it('should open dialog if error occured', () => {
     actions$ = of(displayServerError);
     effects.displayErrorEffect$.subscribe(() => {
-      expect()
-    })
-  })
-})
+      expect(snackBar.open).toBeCalled();
+    });
+  });
+
+  it('should dispatch startLoading and get uploaded images on syncState', () => {
+    effects.displayErrorEffect$.subscribe(() => {
+      expect(store.dispatch).toBeCalled();
+      expect(uploadService.getUploadedImages()).toBeCalled();
+    });
+  });
+});
